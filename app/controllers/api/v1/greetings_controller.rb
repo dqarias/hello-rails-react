4439c9b4e93942f7d07d7dd958d751e5922ce512
dqarias <dino.quispea@gmail.com>
2022-10-18 19:49:37 -0500

    Fix linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::GreetingsController < ApplicationController
-    def index
-        offset = rand(Greeting.count)
-        @greeting_random = Greeting.offset(offset).first
-        render json: @greeting_random
-    end
-    
+  def index
+    offset = rand(Greeting.count)
+    @greeting_random = Greeting.offset(offset).first
+    render json: @greeting_random
+  end
 end
